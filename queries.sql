3249efa24e46e803764f0e66a25b9714705c56ba
limir28 <Lizamiron28@gmail.com>
2024-09-17 01:31:22 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,7 +6,7 @@ from customers;
 select
     concat(e.first_name, ' ', e.last_name) as seller,
     count(s.quantity) as operations,
-    sum(s.quantity * p.price) as income
+    floor(sum(s.quantity * p.price)) as income
 from employees as e
 inner join sales as s on e.employee_id = s.sales_person_id
 inner join products as p on s.product_id = p.product_id
@@ -18,14 +18,14 @@ WITH individual_income AS (
     SELECT
         e.employee_id,
         CONCAT(e.first_name, ' ', e.last_name) AS seller,
-        AVG(s.quantity * p.price) AS average_income
+        FLOOR(AVG(s.quantity * p.price)) AS average_income
     FROM employees AS e
     INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
     INNER JOIN products AS p ON s.product_id = p.product_id
     GROUP BY e.employee_id, seller
 ),
 overall_avg AS (
-    SELECT AVG(average_income) AS overall_avg_income
+    SELECT FLOOR(AVG(average_income)) AS overall_avg_income
     FROM individual_income
 )
 SELECT
@@ -35,4 +35,3 @@ FROM individual_income AS ii,
     overall_avg AS oa
 WHERE ii.average_income < oa.overall_avg_income
 ORDER BY ii.average_income ASC;
-
